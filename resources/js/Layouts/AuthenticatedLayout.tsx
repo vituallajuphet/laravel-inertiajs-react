c8cd18d61308ccaff9d7784338aecfade2067f9c
Juphet Vitualla <jvitualla@dnamicro.com>
2023-10-19 04:40:15 +0800

    udpate

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -141,7 +141,9 @@ export default function Authenticated(props: any) {
                                                         <span className="text-gray-500">Janitor</span>
                                                     </div>
                                                     <div className="flex items-center ml-4">
-                                                        <div className="rounded-full w-12 h-12 bg-red-200 mr-2"></div>
+                                                        <div className="rounded-full w-12 h-12 bg-red-200 mr-2">
+                                                         <img src="https://demo.tailadmin.com/src/images/user/user-02.png"/>
+                                                        </div>
                                                         <FaChevronDown />
                                                     </div>
                                                 </div>
@@ -170,7 +172,62 @@ export default function Authenticated(props: any) {
                     </div>
                     <div className="flex-1 flex overflow-y-scroll">
                         <main className="h-[2000px] w-full p-8">
-                            <div>Content here</div>
+                            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-200">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            Product ID
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Product Name
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Price
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Created Date
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Last Updated
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            <span className="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* {products?.map((prod) => {
+                        return (
+                            <tr key={prod.product_id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <th
+                                    scope="row"
+                                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                >
+                                    {prod.product_id}
+                                </th>
+                                <td className="px-6 py-4">{prod.product_name}</td>
+                                <td className="px-6 py-4">${prod.price}</td>
+                                <td className="px-6 py-4">{
+                                    moment(prod.created_at, "YYYYMMDD").fromNow()
+                                }</td>
+                                <td className="px-6 py-4">{
+                                    moment(prod.updated_at, "YYYYMMDD").fromNow()
+                                }</td>
+                                <td className="px-6 py-4 text-right">
+                                    <div>   
+                                        <Link
+                                            href={`${url}/store/${store_id}/product/${prod.product_id}`}
+                                            className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                                        >
+                                            View
+                                        </Link>
+                                    </div>
+                                </td>
+                            </tr>
+                        );
+                    })} */}
+                </tbody>
+            </table>
                         </main>
                     </div>
                 </div>
